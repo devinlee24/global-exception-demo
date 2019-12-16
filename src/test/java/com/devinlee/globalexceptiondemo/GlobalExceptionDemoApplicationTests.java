93f67fd998bd915d2bfb9c2b198e2892333e90d9
devinlee <devinlee24@foxmail.com>
2019-12-16 18:45:29 +0800

    初始化

---
+++ src/test/java/com/devinlee/globalexceptiondemo/GlobalExceptionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devinlee.globalexceptiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalExceptionDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
